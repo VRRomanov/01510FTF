18f63873e8f848a5145ceb0ff5b84b22194c78c9
VRRomanov <sasanikolai@yandex.ru>
2023-09-20 18:15:20 +0300

    Add AppDomain

--- 01510FTF/Program.cs
+++ 01510FTF/Program.cs
@@ -1,7 +1,22 @@
-﻿
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Net;
+using System.Net.WebSockets;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        build: MaskedTextProvider = new MaskedTextProvider
+
+        test: AppDomain = new AppDomain
 
 
-asdasd
+
+
+
 
 
 
